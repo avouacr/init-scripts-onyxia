4d3683bcd3a847d58d7f11ac76fe07575b58dc87
Romain Avouac <43444134+avouacr@users.noreply.github.com>
2023-05-02 13:54:33 +0200

    Create init-catalog.sh

---
+++ init-catalog.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://$GIT_PERSONAL_ACCESS_TOKEN@github.com/InseeFrLab/images-datascience.git
+git clone https://$GIT_PERSONAL_ACCESS_TOKEN@github.com/InseeFrLab/helm-charts-interactive-services.git
+git clone https://$GIT_PERSONAL_ACCESS_TOKEN@github.com/InseeFrLab/helm-charts-automation.git
+git clone https://$GIT_PERSONAL_ACCESS_TOKEN@github.com/InseeFrLab/helm-charts-databases.git
