65a83fc6d2bb925c94bfe23253e026f21c58897f
Romain Avouac <43444134+avouacr@users.noreply.github.com>
2023-10-26 18:46:35 +0200

    Update init-catalog.sh

--- init-catalog.sh
+++ init-catalog.sh
@@ -4,6 +4,7 @@ git clone https://$GIT_PERSONAL_ACCESS_TOKEN@github.com/InseeFrLab/images-datasc
 git clone https://$GIT_PERSONAL_ACCESS_TOKEN@github.com/InseeFrLab/helm-charts-interactive-services.git
 git clone https://$GIT_PERSONAL_ACCESS_TOKEN@github.com/InseeFrLab/helm-charts-automation.git
 git clone https://$GIT_PERSONAL_ACCESS_TOKEN@github.com/InseeFrLab/helm-charts-databases.git
+git clone https://$GIT_PERSONAL_ACCESS_TOKEN@github.com/InseeFrLab/helm-charts-datavisualization.git
 git clone https://$GIT_PERSONAL_ACCESS_TOKEN@github.com/InseeFrLab/helm-charts.git
 git clone https://$GIT_PERSONAL_ACCESS_TOKEN@github.com/InseeFrLab/helm-charts-dev.git
 git clone https://$GIT_PERSONAL_ACCESS_TOKEN@github.com/InseeFrLab/www.sspcloud.fr.git
