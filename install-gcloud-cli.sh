97ae33a3b71bc62e5d2ae411189bc78f38d6ec04
avouacr <avouacr@pm.me>
2024-02-20 17:49:20 +0000

    fix init

--- install-gcloud-cli.sh
+++ install-gcloud-cli.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
 
-curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-464.0.0-linux-x86_64.tar.gz
-tar -xf google-cloud-cli-464.0.0-linux-x86_64.tar.gz
-./google-cloud-sdk/install.sh --command-completion true --path-update true --quiet
-rm -rf google-cloud-sdk/ google-cloud-cli-464.0.0-linux-x86_64.tar.gz
+curl https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-464.0.0-linux-x86_64.tar.gz -o $WORKSPACE_DIR/gcloud-cli.tar.gz
+tar -xf $WORKSPACE_DIR/gcloud-cli.tar.gz
+rm $WORKSPACE_DIR/gcloud-cli.tar.gz
 
-source ~/.bashrc
-gcloud auth login
+$WORKSPACE_DIR/google-cloud-sdk/install.sh --command-completion true --path-update true --quiet
+$WORKSPACE_DIR/google-cloud-sdk/bin/gcloud auth login
