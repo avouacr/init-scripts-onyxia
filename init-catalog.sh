051edf01ed52c23353319289f2d712b22c6b9053
Romain Avouac <43444134+avouacr@users.noreply.github.com>
2024-02-20 15:44:24 +0100

    gcloud

--- init-catalog.sh
+++ init-catalog.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+set -e
 
+# Clone Onyxia's repositories
 git clone https://$GIT_PERSONAL_ACCESS_TOKEN@github.com/InseeFrLab/images-datascience.git
 git clone https://$GIT_PERSONAL_ACCESS_TOKEN@github.com/InseeFrLab/helm-charts-interactive-services.git
 git clone https://$GIT_PERSONAL_ACCESS_TOKEN@github.com/InseeFrLab/helm-charts-automation.git
@@ -7,3 +9,8 @@ git clone https://$GIT_PERSONAL_ACCESS_TOKEN@github.com/InseeFrLab/helm-charts-d
 git clone https://$GIT_PERSONAL_ACCESS_TOKEN@github.com/InseeFrLab/helm-charts-datavisualization.git
 git clone https://$GIT_PERSONAL_ACCESS_TOKEN@github.com/InseeFrLab/helm-charts.git
 git clone https://$GIT_PERSONAL_ACCESS_TOKEN@github.com/InseeFrLab/helm-charts-dev.git
+
+# Install gcloud CLI
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-464.0.0-linux-x86_64.tar.gz
+tar -xf google-cloud-cli-464.0.0-linux-x86_64.tar.gz
+./google-cloud-sdk/install.sh --command-completion true --path-update true --quiet
