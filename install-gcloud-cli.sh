d66b77f5617fbcfe05aa73658e8531bd419e0b49
avouacr <avouacr@pm.me>
2024-06-19 11:31:28 +0000

    permissions gcloud

--- install-gcloud-cli.sh
+++ install-gcloud-cli.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Run using sudo -E install-gcloud-cli.sh
 set -e
 
 curl https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-464.0.0-linux-x86_64.tar.gz -o $WORKSPACE_DIR/gcloud-cli.tar.gz
@@ -7,3 +8,4 @@ rm $WORKSPACE_DIR/gcloud-cli.tar.gz
 
 $WORKSPACE_DIR/google-cloud-sdk/install.sh --command-completion true --path-update true --quiet
 $WORKSPACE_DIR/google-cloud-sdk/bin/gcloud auth login
+chown -R ${USERNAME}:${GROUPNAME} $HOME/.config/gcloud/
